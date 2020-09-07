d5146e0dbc8e0c0de3dbd20401eb33348eadc6a3
trogmaiu <58095197+trogmaiu@users.noreply.github.com>
2020-09-07 01:41:24 -0300

    Update a.js

--- a.js
+++ a.js
@@ -3,14 +3,22 @@ function iniciaJogo(){
 tempo_segundos = 120;
 document.getElementById('cronometro').innerHTML = tempo_segundos;
 	contagem_tempo(tempo_segundos + 1)
+
 }
 
-
+function carregargift() {
+	window.location.href = 'tenor.gift';
+}
 
 function contagem_tempo(segundos){
 
 	segundos = segundos - 1;
 
+	if(segundos == 0){
+		
+		carregargift();
+	}
+		
 	if(segundos == -1){
 		clearTimeout(timerId); //para a execução da função do settimeout
 		return false;
